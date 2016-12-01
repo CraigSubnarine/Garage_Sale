c08034153bfb918752f5ed4155e2a2d777316e47
CraigSubnarine <craig.subnarine@gmail.com>
2016-12-01 08:52:28 -0400

    sql file
    sql file that I used for testing lib.php

---
+++ garage_sale.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2016 at 01:51 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `garage_sale`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interests`
+--
+
+CREATE TABLE `interests` (
+  `userid` int(11) NOT NULL,
+  `itemid` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `itemid` int(255) NOT NULL,
+  `itemname` varchar(100) NOT NULL,
+  `userid` int(100) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `price` varchar(10) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `interestno` int(55) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`itemid`, `itemname`, `userid`, `type`, `description`, `timestamp`, `price`, `quantity`, `interestno`) VALUES
+(1, 'Pants', 1, 'clothing', 'new and very stylish', '2016-12-01 11:43:26', '10', 10, 0),
+(2, 'T-shirt', 1, 'clothing', 'old levis', '2016-12-01 06:37:51', '4', 10, 0),
+(3, 'Slipper', 2, 'footwear', 'american eagle', '2016-12-01 07:14:55', '2', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contactno` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `username`, `password`, `email`, `contactno`) VALUES
+(1, 'Mike', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'john@gmail.com', 9999999),
+(2, 'Jane', 'a04d0b2919aeb0533c8c370b2fb3d73ea1db8ec7', 'jane@gmail.com', 4444444);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`itemid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `itemid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
