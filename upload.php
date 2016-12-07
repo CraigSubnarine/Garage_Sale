a60a29ffed3bc5c3b4fead711add76d89c0bf4e4
CraigMohammed <craig.mohammed@my.uwi.edu>
2016-12-07 13:08:31 -0400

    writes to DB and uploads

--- upload.php
+++ upload.php
@@ -9,14 +9,17 @@ if(isset($_POST['upload'])){
   $image=$_FILES['image']['name'];
 //  $text=$_POST['text'];
 
-  $sql="INSERT into `images` (`name`)VALUES ($image);";
+  $sql="INSERT INTO `images` (`id`, `name`, `timestamp`) VALUES (NULL, '$image', CURRENT_TIMESTAMP);";
+
   $res=$db->query($sql);
 
   if(move_uploaded_file($_FILES['image']['tmp_name'],$target))
   {
     echo "IMAGE UPLOADED Successfully";
   }
-
+  else {
+    echo "Image not uploaded!! ";
+  }
 }
 ?>
 
